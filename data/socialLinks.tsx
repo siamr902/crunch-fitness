2755653f5fa1101c4a67d5e8bac9bdaa067d0692
Siam Rahman <siamr.bro99@gmail.com>
2022-12-04 23:12:40 -0500

    Testing to see if new url works

--- data/socialLinks.tsx
+++ data/socialLinks.tsx
@@ -5,6 +5,7 @@ const socialLinks = {
     instagram: "https://www.instagram.com/crunch.chamblee/?hl=en",
 }
 
+// for payment section
 export const termLink: string = "https://d3gwr21bcravq3.cloudfront.net/store/club/membership-agreement-f2814ef0271510a6495b.pdf"
 
 export default socialLinks
